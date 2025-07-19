c47aab617136ad9320fe959a60bc943e90eb268c
Renzo Deiparine <renzomiguel.deiparine.22@usjr.edu.ph>
2025-07-19 01:41:52 -0700

    Image fit

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -2,7 +2,7 @@
     <a href="{{ route('details', $item->id) }}" class="text-decoration-none">
         <div class="card h-100 shadow-sm">
             @if (!empty($item->image))
-                <img src="{{ \Illuminate\Support\Facades\Storage::url($item->image) }}" class="card-img-top" alt="{{ $item->name }}" style="object-fit: cover; height: 200px; width: 100%;">
+                <img src="{{ \Illuminate\Support\Facades\Storage::url($item->image) }}" class="card-img-top" alt="{{ $item->name }}" style="auto-fit: cover; height: 200px; width: 100%;">
             @else
                 <div class="card-img-top d-flex align-items-center justify-content-center bg-secondary text-white" style="height: 200px;">
                     No Image
